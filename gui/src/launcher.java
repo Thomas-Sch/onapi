a37b42b5e06fa65b70cb85f8845fc4eb664d5468
Thomas <thms.sch@gmail.com>
2013-05-01 13:46:42 +0200

    Init commit GUI

---
+++ gui/src/launcher.java
@@ -0,0 +1,30 @@
+/* ============================================================================
+ * Nom du fichier   : launcher.java
+ * ============================================================================
+ * Date de création : 1 mai 2013
+ * ============================================================================
+ * Auteurs          : Crescenzio Fabio
+ *                    Decorvet Grégoire
+ *                    Jaquier Kevin
+ *                    Schweizer Thomas
+ * ============================================================================
+ */
+
+/**
+ * TODO
+ * @author Crescenzio Fabio
+ * @author Decorvet Grégoire
+ * @author Jaquier Kevin
+ * @author Schweizer Thomas
+ *
+ */
+public class launcher {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      System.out.println("Je suis l'interface !");
+   }
+
+}
