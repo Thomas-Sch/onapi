d703343330aa218f623158070bd6921bf97f6142
Thomas <thms.sch@gmail.com>
2013-05-01 13:35:48 +0200

    Init commit

---
+++ client/src/launcher.java
@@ -0,0 +1,31 @@
+/* ============================================================================
+ * Nom du fichier   : launcher.java
+ * ============================================================================
+ * Date de création : 1 mai 2013
+ * ============================================================================
+ * Auteurs          : Crescenzio Fabio
+ *                    Decorvet Grégoire
+ *                    Jaquier Kevin
+ *                    Schweizer Thomas
+ * ============================================================================
+ */
+
+/**
+ * TODO
+ * @author Crescenzio Fabio
+ * @author Decorvet Grégoire
+ * @author Jaquier Kevin
+ * @author Schweizer Thomas
+ *
+ */
+public class launcher {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      System.out.println("Salut, je suis le client.");
+
+   }
+
+}
