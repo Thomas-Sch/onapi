b138d0384b8a8f4c2c2fa182be538c9a759ee492
Thomas <thms.sch@gmail.com>
2013-05-01 14:06:35 +0200

    init commit gui prefs formatter

---
+++ gui/src/gui/JOnapi.java
@@ -0,0 +1,24 @@
+/* ============================================================================
+ * Nom du fichier   : JOnapi.java
+ * ============================================================================
+ * Date de création : 1 mai 2013
+ * ============================================================================
+ * Auteurs          : Crescenzio Fabio
+ *                    Decorvet Grégoire
+ *                    Jaquier Kevin
+ *                    Schweizer Thomas
+ * ============================================================================
+ */
+package gui;
+
+/**
+ * TODO
+ * @author Crescenzio Fabio
+ * @author Decorvet Grégoire
+ * @author Jaquier Kevin
+ * @author Schweizer Thomas
+ *
+ */
+public class JOnapi {
+
+}
