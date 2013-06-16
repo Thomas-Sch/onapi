89548c119812eb26f45659e4d17d8a1596deb7d1
Fabio Crescenzio <fcrescenzio@gmail.com>
2013-06-16 18:24:48 +0200

    Update animation

--- client/client/src/game/models/Player.java
+++ client/client/src/game/models/Player.java
@@ -69,7 +69,7 @@ public class Player extends Entity {
    private boolean isMoving = false;
    private float lastFrameUpdate = 0.0f;
    private float currentTime = 0.0f;
-   private static final int NB_FRAMES_ANIMATION = 5;
+   private static final int NB_FRAMES_ANIMATION = 6;
    private static final float ANIMATION_DURATION = 0.3f; // en [s]
 
    /**
@@ -276,8 +276,8 @@ public class Player extends Entity {
       if (currentTime - lastFrameUpdate > ANIMATION_DURATION
             / NB_FRAMES_ANIMATION) {
          if (isMoving) {
-            animationFrameIndex = (animationFrameIndex + 1)
-                  % NB_FRAMES_ANIMATION;
+            animationFrameIndex = (animationFrameIndex+1)
+                  % (NB_FRAMES_ANIMATION + 1);
             isMoving = false;
          }
          else {
